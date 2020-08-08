3eb512b6c5a5e4eee3644a45ba9bf376a96fba31
Qingchuan Zhu <zqc@utexas.edu>
2020-08-08 15:01:55 -0700

    count vowels frequency

--- HFPython/vowels.py
+++ HFPython/vowels.py
@@ -1,7 +1,14 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
-found = []
 word = input("Provide a word to search for vowels:")
+found= {}
+found['a'] = 0
+found['e'] = 0
+found['i'] = 0
+found['o'] = 0
+found['u'] = 0
+
 for letter in word:
-    if letter in vowels and letter not in found:
-        found.append(letter)
-print(found)
+    if letter in vowels:
+        found[letter] += 1
+for k,v in sorted(found.items()):
+    print(k, 'was found', v, 'time(s)')
