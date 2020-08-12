9bf2e1a6ca03ceda12cdcb6fcc2224105dbc5673
Qingchuan Zhu <zqc@utexas.edu>
2020-08-11 18:41:14 -0700

    create search for letters function

--- HFPython/vsearch.py
+++ HFPython/vsearch.py
@@ -1,7 +1,8 @@
 def search4vowels(word:str) -> set:
     """Display any vowels found in a supplied word."""
     vowels = set('aeiou')
-    found = vowels.intersection(set(word))
-    for letter in found:
-        print(letter)
+    return vowels.intersection(set(word))
     
+def search4letters(phrase:str='hello world', letters:str='aeiou') -> set:
+    """Return a set of the 'letters' found in 'phrase'"""
+    return set(letters).intersection(set(phrase))
