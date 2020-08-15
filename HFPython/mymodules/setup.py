084d139589f5ea8f96a8d9eb10e27b65a9c44d68
Qingchuan Zhu <zqc@utexas.edu>
2020-08-15 14:20:34 -0700

    Prepare for the creation of package

---
+++ HFPython/mymodules/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'vsearch',
+    version = '1.0',
+    description = 'The Head First Python Search Tools',
+    author = 'HF Python',
+    py_modules=['vsearch'],
+)
