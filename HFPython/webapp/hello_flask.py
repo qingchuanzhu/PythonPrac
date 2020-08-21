7634da383e6066b0cce6cb07ec9e13ee14ba3912
Qingchuan Zhu <zqc@utexas.edu>
2020-08-20 17:37:20 -0700

    Added search4 path

--- HFPython/webapp/hello_flask.py
+++ HFPython/webapp/hello_flask.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from vsearch import search4letters
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello() -> str:
     return 'Hello world from Flask!'
+
+@app.route('/search4')
+def do_search():
+    return str(search4letters(phrase='life, the universe, and everything',
+                          letters='eiru,!'))
 
 app.run()
