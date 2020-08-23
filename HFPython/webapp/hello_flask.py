7ca536f1d53ecfece007976ea822960c7874dbc3
Qingchuan Zhu <zqc@utexas.edu>
2020-08-23 15:33:55 -0700

    render the entry page

--- HFPython/webapp/hello_flask.py
+++ HFPython/webapp/hello_flask.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from vsearch import search4letters
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -7,9 +8,13 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello world from Flask!'
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search():
     return str(search4letters(phrase='life, the universe, and everything',
                           letters='eiru,!'))
 
-app.run()
+@app.route('/entry')
+def entry_page():
+    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+app.run(debug=True)
