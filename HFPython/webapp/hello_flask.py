1d7143e2f4a9e1938e89733639401bc38ea3939e
Qingchuan Zhu <zqc@utexas.edu>
2020-08-26 20:24:32 -0700

    redirect / to /entry

--- HFPython/webapp/hello_flask.py
+++ HFPython/webapp/hello_flask.py
@@ -4,10 +4,6 @@ from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
 @app.route('/search4', methods=['POST'])
 def do_search():
     phrase = request.form['phrase']
@@ -17,7 +13,7 @@ def do_search():
                            the_phrase=phrase,
                            the_letters=letters,
                            the_results=str(search4letters(phrase=phrase,letters=letters)))
-
+@app.route('/')
 @app.route('/entry')
 def entry_page():
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
