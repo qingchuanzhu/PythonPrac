8e1ed0c97954352f4221276d77c19a59fd4d7a07
Qingchuan Zhu <zqc@utexas.edu>
2020-08-27 21:26:40 -0700

    render log in html

--- HFPython/webapp/hello_flask.py
+++ HFPython/webapp/hello_flask.py
@@ -1,12 +1,11 @@
-from flask import Flask
 from vsearch import search4letters
-from flask import render_template, request
+from flask import Flask, render_template, request, escape
 
 app = Flask(__name__)
 
 def log_request(req:'flask_request', res:str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 @app.route('/search4', methods=['POST'])
 def do_search():
@@ -25,10 +24,18 @@ def entry_page():
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
+    contents=[]
     with open('vsearch.log') as log:
-        contents = log.read()
-    return contents
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles=('Form Data', 'Remote Addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
     
 if __name__ == '__main__':
     app.run(debug=True)
